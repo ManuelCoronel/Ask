91a6308c25eac388a9158326f695993d8c9a82d0
ManuelCoronelAndrade <andrademanuelalejandroc@ufps.edu.co>
2022-06-05 18:06:51 -0500

    New training front agregado

--- Ask/app/src/main/java/com/example/ask/DashboardActivity.kt
+++ Ask/app/src/main/java/com/example/ask/DashboardActivity.kt
@@ -14,16 +14,26 @@ class DashboardActivity : AppCompatActivity() {
     private lateinit var mAuth: FirebaseAuth
     private lateinit var nombre: TextView
     private lateinit var salir: ImageView
+    private lateinit var  btnTraining : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
         nombre = findViewById(R.id.textView9)
         salir = findViewById(R.id.btn_salir)
+        btnTraining = findViewById(R.id.botonTraining)
         salir.setOnClickListener {
             salir()
         }
+        btnTraining.setOnClickListener{
+            irTraining()}
         initialise()
+    }
+
+    private fun irTraining(){
+        val intent = Intent(this, NewTrainingActivity::class.java)
+        startActivity(intent)
+
     }
 
     private fun initialise() {
