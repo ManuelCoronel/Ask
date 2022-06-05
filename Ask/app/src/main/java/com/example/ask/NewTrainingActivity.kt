6271bd884c0b5b5e3f2bf54586f3222f0a1fa3ab
ManuelCoronelAndrade <andrademanuelalejandroc@ufps.edu.co>
2022-06-05 18:35:11 -0500

    Cambios menores front new training

--- Ask/app/src/main/java/com/example/ask/NewTrainingActivity.kt
+++ Ask/app/src/main/java/com/example/ask/NewTrainingActivity.kt
@@ -1,11 +1,29 @@
 package com.example.ask
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class NewTrainingActivity : AppCompatActivity() {
+
+    private lateinit var btn: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_training)
+
+        btn = findViewById(R.id.btnBack)
+
+        btn.setOnClickListener{
+            irTraining()
+        }
+
+    }
+
+    private fun irTraining(){
+        val intent = Intent(this, DashboardActivity::class.java)
+        startActivity(intent)
+
     }
 }
